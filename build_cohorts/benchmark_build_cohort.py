73c9d9bf36bec6c5b71135c13d6b8e4405110e07
Giorgia Gandolfi <59025266+giorgiagandolfi@users.noreply.github.com>
2025-07-17 14:44:06 +0200

    Change reading of cancer_type file in benchmark_build_cohort.py

--- build_cohorts/benchmark_build_cohort.py
+++ build_cohorts/benchmark_build_cohort.py
@@ -958,8 +958,6 @@ if (__name__ == '__main__'):
 
     subprocess.run(cmd)
 
-    with open(tumour_type_file) as cancer_type_file:
-        cancer_type = cancer_type_file.read().strip()
 
     
     config_file = args.config
@@ -1156,6 +1154,8 @@ if (__name__ == '__main__'):
                     sequenza_launcher = sequenza_launcher_orig
                     
                     #tumourevo sh file and csv file
+		    with open(tumour_type_file) as cancer_type_file:
+			cancer_type = cancer_type_file.read().strip()
                     variant_callers = ['freebayes', 'strelka', 'mutect2']
                     cn_caller = ['ascat', 'sequenza']
                     combinations = []
