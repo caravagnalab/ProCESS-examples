c062586079244f5b2a9ac0853813a5dd894f3224
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2025-07-29 10:20:18 +0200

    add path to context matrix in get_tumourevo_signatures getter

--- getters/tumourevo_getters.R
+++ getters/tumourevo_getters.R
@@ -336,9 +336,10 @@ get_tumourevo_signatures <- function(
   
   
   if (tool == "SigProfiler") {
+    signature_class = gsub('[[:digit:]]+', '', context) 
+    context_matrix <- file.path(MAIN_PATH, tool,"SCOUT", "results","output",signature_class,paste0("SCOUT.",context,".all"))
     
     MAIN_PATH <- file.path(MAIN_PATH, tool, "SCOUT", "results", context, context, "Suggested_Solution")
-    
     COSMIC <- file.path(MAIN_PATH, paste0("COSMIC_", context, "_Decomposed_Solution"))
     COSMIC_exposure <- paste0(COSMIC, "/Activities/COSMIC_", context, "_Activities.txt")
     COSMIC_signatures <- paste0(COSMIC, "/Signatures/COSMIC_", context, "_Signatures.txt")
@@ -348,6 +349,7 @@ get_tumourevo_signatures <- function(
     denovo_signatures <- paste0(denovo, "/Signatures/", context, "_De-Novo_Signatures.txt")
     
     return(list(
+      context_matrix=context_matrix,
       COSMIC_exposure=COSMIC_exposure, 
       COSMIC_signatures=COSMIC_signatures, 
       denovo_exposure=denovo_exposure, 
