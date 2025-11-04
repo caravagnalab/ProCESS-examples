f47d687bb11739a85ff30838866c4400f072a776
valerianilucrezia <valerianilucrezia11@gmail.com>
2025-11-04 15:48:27 +0100

    add new figures code

---
+++ figures/cna/cna_breakpoints.R
@@ -0,0 +1,92 @@
+rm(list=ls())
+library(ProCESS)
+library(ggplot2)
+library(dplyr)
+library(colormap)
+library(RColorBrewer)
+library(patchwork)
+library(ComplexHeatmap)
+source("/orfeo/cephfs/scratch/cdslab/ggandolfi/Github/ProCESS-examples/getters/process_getters.R")
+# source("compute_FGA.R")
+scout_dir <-"/orfeo/cephfs/scratch/cdslab/shared/SCOUT/"
+SPNS <- c("SPN01","SPN02","SPN03","SPN04", "SPN06", 'SPN07')
+COVERAGES <- c("50","100", "150")
+PURITIES <- c("0.3","0.6","0.9")
+SPN_colors <-c("SPN01"='steelblue', "SPN02"='seagreen', "SPN03"='goldenrod', 
+               "SPN04"='coral', "SPN06"='palevioletred', "SPN07"='indianred3')
+source("/orfeo/cephfs/scratch/cdslab/ggandolfi/Github/ProCESS-examples/validation/SCOUT/colors.R")
+WGD_samples <- c("SPN01_1.1","SPN01_1.3","SPN06_3.1","SPN06_3.2")
+
+validation_dir <- "/orfeo/cephfs/scratch/cdslab/shared/SCOUT/VALIDATION"
+params_grid = expand.grid(COVERAGES, PURITIES)
+colnames(params_grid) = c("coverage", "purity")
+df_all_SPN <- list()
+for (SPN in SPNS){
+  spn <- SPN
+  df = lapply(1:nrow(params_grid), function(i) {
+    coverage = params_grid[i,]$coverage
+    purity = params_grid[i,]$purity
+    comb <- paste0(coverage,"x_",purity)
+    samples <- get_sample_names(spn = spn)
+    validation_dir_cna <- file.path(validation_dir,spn,"cna")
+    all_metrics_comb <- list()
+    for (sample in samples){
+      metrics_filename <- paste0(validation_dir_cna,"/",comb,"/",sample,"/metrics.rds")
+      metrics_bp_filename <- paste0(validation_dir_cna,"/",comb,"/",sample,"/metrics_bp.rds")
+      if (!file.exists(metrics_filename)){
+        message("File not found: ", metrics_filename)
+        # all_metrics_comb[[sample]] <- NA
+      } else{
+        metrics_df <- readRDS(metrics_filename) %>% 
+          mutate(delta_purity=as.numeric(true_purity)-as.numeric(purity)) %>% 
+          mutate(delta_ploidy=as.numeric(true_ploidy)-as.numeric(ploidy)) %>% 
+          filter(tool!="cnvkit")
+        metrics_bp_df <- readRDS(metrics_bp_filename) %>% 
+          filter(chr=="genome") %>% 
+          filter(tool!="cnvkit")
+        all_metrics_comb[[sample]] <- inner_join(metrics_df,metrics_bp_df,by=c("tool","sample","spn","coverage","fga","fgs","true_purity"))
+      }
+    }
+    all_combinations_SPN <- do.call("rbind",all_metrics_comb)
+  })
+  df_all_SPN[[spn]] <- do.call("rbind",df)  
+}
+df_all_combs_SPN <- do.call("rbind",df_all_SPN)
+df_all_combs_SPN <- df_all_combs_SPN %>% mutate(tool = case_when(
+  tool == 'ascat' ~ 'ASCAT',
+  tool == 'sequenza' ~ 'Sequenza',
+  tool == 'battenberg' ~ 'Battenberg',
+))
+
+
+
+plt <- df_all_combs_SPN %>%
+  ggplot() +
+  geom_boxplot(aes(x = tool, y = correctness_clonal*100, fill = tool)) +
+  scale_fill_manual('Caller',values = c('deepskyblue4', 'maroon', 'sienna')) +
+  theme_minimal() +
+  xlab('') +
+  ylab('% correcly identified clonal CN') +
+  
+  df_all_combs_SPN %>%
+  ggplot() +
+  geom_boxplot(aes(x = tool, y = precision, fill = tool)) +
+  scale_fill_manual('Caller', values = c('deepskyblue4', 'maroon', 'sienna')) +
+  theme_minimal()  +
+  #facet_grid(true_purity ~ coverage) +
+  ylim(0,1) +
+  ylab('Breakpoint precision') +
+  xlab('') +
+  
+  df_all_combs_SPN %>%
+  ggplot() +
+  geom_boxplot(aes(x = tool, y = recall, fill = tool)) +
+  scale_fill_manual('Caller',values = c('deepskyblue4', 'maroon', 'sienna')) +
+  theme_minimal() +
+  ylim(0,1) +
+  #facet_grid(true_purity ~ coverage) +
+  ylab('Breakpoint sensitivity') +
+  xlab('') +
+  plot_layout(guides = 'collect') + plot_annotation(tag_levels = 'A') & theme(legend.position = 'bottom')
+ggsave(filename = "cna_stat.pdf",plot = plt, width = 9,height = 3)
+
--- figures/somatic/somatic.R
+++ figures/somatic/somatic.R
@@ -1,45 +1,45 @@
 library(tidyverse)
 library(patchwork)
 
-# base = "/orfeo/scratch/cdslab/shared/SCOUT/VALIDATION/"
-# SPNS <- paste0('SPN0', c(1,2,3,4,7))
-# CALLERS <- c('mutect2', 'strelka', 'freebayes')
-# COVERAGES <- c(50,100,150)
-# PURITY <- c(0.3, 0.6, 0.9)
-# MUTS <- c('SNV', 'INDEL')
-# 
-# params_grid = expand.grid(COVERAGES, PURITY, MUTS, SPNS)
-# names(params_grid) <- c('cov', 'pur', 'muts', 'spn')
-# 
-# df = lapply(1:nrow(params_grid), function(i) {
-#   mut_type = params_grid[i,]$muts
-#   coverage = params_grid[i,]$cov
-#   purity = params_grid[i,]$pur
-#   spn = params_grid[i,]$spn
-#   
-#   file =  paste0('/orfeo/scratch/cdslab/shared/SCOUT/VALIDATION/', spn ,'/somatic/', coverage,'x_',purity,'p/report/',mut_type,'/metrics.rds')
-#   
-#   if (file.exists(file)) {
-#     metrics = readRDS(file)
-#     
-#     parsed_metrics = lapply(names(metrics), function(caller_name) {
-#       caller_metrics = metrics[[caller_name]]
-#       sample_names = names(caller_metrics)
-#       lapply(sample_names, function(sample_id) {
-#         FP = caller_metrics[[sample_id]]$detection_summary["False Positive"]
-#         d = caller_metrics[[sample_id]]$performance_table %>% 
-#           dplyr::mutate(sample_id = sample_id, SPN = spn) %>%  
-#           dplyr::mutate(purity = as.numeric(purity), coverage = as.numeric(coverage))
-#         d$false_positive[1] = FP
-#         d
-#       }) %>% do.call("bind_rows", .) %>% 
-#         dplyr::mutate(caller = caller_name)
-#     }) %>% do.call("bind_rows", .)
-#     
-#     dplyr::bind_cols(parsed_metrics, params_grid[i,])
-#   }
-# }) %>% do.call("bind_rows", .)
-# saveRDS(object = df, file = '/orfeo/cephfs/scratch/area/lvaleriani/races/ProCESS-examples/figures/somatic/somatic.rds')
+base = "/orfeo/scratch/cdslab/shared/SCOUT/VALIDATION/"
+SPNS <- paste0('SPN0', c(1,2,3,4,6,7))
+CALLERS <- c('mutect2', 'strelka', 'freebayes')
+COVERAGES <- c(50,100,150)
+PURITY <- c(0.3, 0.6, 0.9)
+MUTS <- c('SNV', 'INDEL')
+
+params_grid = expand.grid(COVERAGES, PURITY, MUTS, SPNS)
+names(params_grid) <- c('cov', 'pur', 'muts', 'spn')
+
+df = lapply(1:nrow(params_grid), function(i) {
+  mut_type = params_grid[i,]$muts
+  coverage = params_grid[i,]$cov
+  purity = params_grid[i,]$pur
+  spn = params_grid[i,]$spn
+
+  file =  paste0('/orfeo/scratch/cdslab/shared/SCOUT/VALIDATION/', spn ,'/somatic/', coverage,'x_',purity,'p/report/',mut_type,'/metrics.rds')
+
+  if (file.exists(file)) {
+    metrics = readRDS(file)
+
+    parsed_metrics = lapply(names(metrics), function(caller_name) {
+      caller_metrics = metrics[[caller_name]]
+      sample_names = names(caller_metrics)
+      lapply(sample_names, function(sample_id) {
+        FP = caller_metrics[[sample_id]]$detection_summary["False Positive"]
+        d = caller_metrics[[sample_id]]$performance_table %>%
+          dplyr::mutate(sample_id = sample_id, SPN = spn) %>%
+          dplyr::mutate(purity = as.numeric(purity), coverage = as.numeric(coverage))
+        d$false_positive[1] = FP
+        d
+      }) %>% do.call("bind_rows", .) %>%
+        dplyr::mutate(caller = caller_name)
+    }) %>% do.call("bind_rows", .)
+
+    dplyr::bind_cols(parsed_metrics, params_grid[i,])
+  }
+}) %>% do.call("bind_rows", .)
+saveRDS(object = df, file = '/orfeo/cephfs/scratch/area/lvaleriani/races/ProCESS-examples/figures/somatic/somatic.rds')
 
 df <- readRDS('/orfeo/cephfs/scratch/area/lvaleriani/races/ProCESS-examples/figures/somatic/somatic.rds')
 
@@ -100,7 +100,7 @@ caller_line <- df %>%
   theme(axis.text.x = element_text(angle = 30, vjust = 1))#, hjust=1))
 
 line <- caller_line + purity_line + coverage_line + plot_layout(nrow = 3) + plot_annotation(tag_levels = 'A')
-ggsave(plot = line, filename = '/orfeo/cephfs/scratch/area/lvaleriani/tesi/sarek/all_sarek.pdf',width = 6, height = 8, units = 'in')
+#ggsave(plot = line, filename = '/orfeo/cephfs/scratch/area/lvaleriani/tesi/sarek/all_sarek.pdf',width = 6, height = 8, units = 'in')
 
 
 tmp <- df %>% 
@@ -151,25 +151,25 @@ coverage <- tmp %>%
   theme(axis.text.x = element_text(angle = 30, vjust = 1.2, hjust=1))
 
 boxplot <- caller + purity + coverage + plot_layout(nrow = 3)
-
-tmp_fp <- df %>% 
-  dplyr::mutate(CCF_bin = case_when(
-    CCF_bin == "Clonal" ~ "Clonal",
-    CCF_bin %in% c("10-25%", "25-50%", "50-99%") ~ "10-99%",
-    TRUE ~ "0-10%"
-  )) %>% 
-  select(sample_id, CCF_bin, pur, spn, cov, muts, false_positive, true_positives, caller) %>% 
-  group_by(sample_id, CCF_bin, pur, spn, cov, muts, caller) %>%
-  dplyr::summarise(FP = sum(false_positive, na.rm = TRUE), TP = sum(true_positives, na.rm = TRUE)) %>% 
-  dplyr::mutate(FDR = FP / (TP + FP))
-
-fp <- tmp_fp %>% 
-  dplyr::group_by(caller, muts, cov, pur, spn) %>% 
-  dplyr::select(FDR, caller) %>% 
-  dplyr::mutate(normalized_fdr = FDR / max(FDR)) %>% 
-  ggplot(mapping = aes(x = CCF_bin, y = normalized_fdr, fill = caller)) +
-  geom_boxplot() + 
-  facet_wrap(~muts) +
-  theme_minimal() +
-  scale_fill_manual(values = method_colors) +
-  labs(x = "Caller", y = "False Discovery Rate", fill = "Caller")
+# 
+# tmp_fp <- df %>% 
+#   dplyr::mutate(CCF_bin = case_when(
+#     CCF_bin == "Clonal" ~ "Clonal",
+#     CCF_bin %in% c("10-25%", "25-50%", "50-99%") ~ "10-99%",
+#     TRUE ~ "0-10%"
+#   )) %>% 
+#   select(sample_id, CCF_bin, pur, spn, cov, muts, false_positive, true_positives, caller) %>% 
+#   group_by(sample_id, CCF_bin, pur, spn, cov, muts, caller) %>%
+#   dplyr::summarise(FP = sum(false_positive, na.rm = TRUE), TP = sum(true_positives, na.rm = TRUE)) %>% 
+#   dplyr::mutate(FDR = FP / (TP + FP))
+# 
+# fp <- tmp_fp %>% 
+#   dplyr::group_by(caller, muts, cov, pur, spn) %>% 
+#   dplyr::select(FDR, caller) %>% 
+#   dplyr::mutate(normalized_fdr = FDR / max(FDR)) %>% 
+#   ggplot(mapping = aes(x = CCF_bin, y = normalized_fdr, fill = caller)) +
+#   geom_boxplot() + 
+#   facet_wrap(~muts) +
+#   theme_minimal() +
+#   scale_fill_manual(values = method_colors) +
+#   labs(x = "Caller", y = "False Discovery Rate", fill = "Caller")
